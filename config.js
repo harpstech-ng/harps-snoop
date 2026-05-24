8c77290ee1ddd44e79e6c353953aeedf65938f66
Harps Tech <okugbepraise@gmail.com>
2026-05-24 01:15:07 +0100

    Add Cloudinary and Firebase configuration

---
+++ config.js
@@ -0,0 +1,12 @@
+export const CLOUDINARY_CLOUD_NAME = "dwc2sthsd"; // Replace this
+export const CLOUDINARY_UPLOAD_PRESET = "phone_catcher_unsigned";
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyAd0fSfV1jlSVCLX5-zK68T7J12yMeSXy4",
+    authDomain: "harps-snoop-catcher-e3a6f.firebaseapp.com",
+    projectId: "harps-snoop-catcher-e3a6f",
+    storageBucket: "harps-snoop-catcher-e3a6f.firebasestorage.app",
+    messagingSenderId: "29500172688",
+    appId: "1:29500172688:web:e2946cdeddc0322c20eb15",
+    measurementId: "G-JCTNNM0PRX"
+};
