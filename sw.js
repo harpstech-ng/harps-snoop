f9124f5f3a6c5f7bb803116a64ab71f7838e0538
Harps Tech <okugbepraise@gmail.com>
2026-05-24 01:17:27 +0100

    Add service worker for caching resources

---
+++ sw.js
@@ -0,0 +1,10 @@
+const CACHE_NAME = "catcher-v1";
+const urlsToCache = ["/index.html", "/dashboard.html", "/config.js", "/manifest.json"];
+
+self.addEventListener("install", event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+});
